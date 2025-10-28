2f9b895cd02fa4f73ed25229f39cfe5119a26caa
alighaderipour <ali.ghaderipour@gmail.com>
2025-10-28 08:56:54 +0330

    init

---
+++ backend/learningconcept/views.py
@@ -0,0 +1,12 @@
+
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import AllowAny
+
+from .models import Category
+from .serializers import CategorySerializer
+
+class CategoryListView(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [AllowAny]
+
---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+import Categories from "@/components/Category.vue";
+</script>
+
+<template>
+<RouterLink to="/">Home</RouterLink>
+  <Categories />
+</template>
+
+<style scoped>
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+</style>
